06d8408cb6a58a0f555fa32b0ed97769734bd7c3
codecreatorsss <codecreatorsss@gmail.com>
2024-03-17 12:38:56 +0530

    Added new base response

--- src/index.ts
+++ src/index.ts
@@ -28,8 +28,8 @@ app.use(express.json({ limit: "10kb" }));
 app.use(cors());
 app.use(morgan("dev"));
 app.use(cookieParser());
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hiiii Hello");
+app.get("/", (_: Request, res: Response) => {
+  res.send("Hello World!");
 });
 connectDB();
 app.use("/api/v1", router);
