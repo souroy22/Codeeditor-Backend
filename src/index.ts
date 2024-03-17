127e8f2c4aafcd1dca83389f17b342b16c2b4e12
codecreatorsss <codecreatorsss@gmail.com>
2024-03-17 12:40:42 +0530

    added cors options

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,7 @@ declare global {
 }
 
 app.use(express.json({ limit: "10kb" }));
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(morgan("dev"));
 app.use(cookieParser());
 app.get("/", (_: Request, res: Response) => {
